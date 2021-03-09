cbda33a3b53b490d14634d06003302b1f3a50e00
Ajay Kumar Singh <a.singh.14@elsevier.com>
2021-03-09 19:40:20 +0530

    added test

--- src/test/java/AZDemo/AppTest.java
+++ src/test/java/AZDemo/AppTest.java
@@ -1,36 +1,15 @@
 package AZDemo;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+public class AppTest {   
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
+    @Test
     public void testApp()
     {
         assertTrue( true );
